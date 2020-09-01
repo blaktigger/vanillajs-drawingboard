39f822af8aa4d1b42232c3a1484f26daf2c5c353
blaktigger <69227197+blaktigger@users.noreply.github.com>
2020-09-01 18:03:24 +0900

    bursh

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
 const canvas = document.getElementById("jsCanvas");
 
+let painting = false;
+
 function onMouseMove(event){
-    const X = event.offsetX;
-    const Y = event.offsetY;
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
 }
 
 function onMouseDown(event){
-    console.log(event);
+    painting = true;
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+function stopPainting(event){
+    painting = false;
 }
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
     canvas.addEventListener("mousedown", onmousedown);
+    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mouseleave", stopPainting);
 }
 
 
