e240fe93f641034b1c70019859b47da5a805c604
blaktigger <69227197+blaktigger@users.noreply.github.com>
2020-09-02 22:28:02 +0900

    errorssu

--- app.js
+++ app.js
@@ -1,37 +1,37 @@
 const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 
-ctx.strockeStyle = "#2c2c2c";
+canvas.width = 500;
+canvas.height = 500;
+
+ctx.strokeStyle = "#2c2c2c";
 ctx.lineWidth = 2.5;
 
 let painting = false;
 
+function stopPainting(){
+    painting = false;
+}
+
+function startPainting(){
+    painting = true;
+}
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
     if(!painting){
         ctx.beginPath();
-        ctx.moveTo(x,y);
+        ctx.moveTo(x, y);
+    } else{
+        ctx.lineTo(x, y);
+        ctx.stroke();
     }
 }
 
-function onMouseDown(event){
+function onMouseDown(event) {
     painting = true;
 }
-
-function onMouseUp(event){
-    stopPainting();
-}
-
-function stopPainting(event){
-    painting = false;
-}
-
-function startPainting(){
-    painting= true
-}
-
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
