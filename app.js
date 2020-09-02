a693ae642df2df094edbfebd1bb0a0ea5f825e9e
blaktigger <69227197+blaktigger@users.noreply.github.com>
2020-09-02 12:13:37 +0900

    path

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strockeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
+
 
 function onMouseMove(event){
     const x = event.offsetX;
     const y = event.offsetY;
-    
+    if(!painting){
+        ctx.beginPath();
+        ctx.moveTo(x,y);
+    }
 }
 
 function onMouseDown(event){
@@ -20,10 +28,15 @@ function stopPainting(event){
     painting = false;
 }
 
+function startPainting(){
+    painting= true
+}
+
+
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
-    canvas.addEventListener("mousedown", onmousedown);
-    canvas.addEventListener("mouseup", onMouseUp);
+    canvas.addEventListener("mousedown", startPainting);
+    canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
 }
 
