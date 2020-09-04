b80f513e97129dba73ee83fda9230997447c4d66
blaktigger <69227197+blaktigger@users.noreply.github.com>
2020-09-04 17:47:37 +0900

    save

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const range = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
 const INITIAL_COLOR = "#2c2c2c";
 const CANAVS_SIZE = 500;
@@ -10,6 +11,8 @@ const CANAVS_SIZE = 500;
 canvas.width = CANAVS_SIZE;
 canvas.height = CANAVS_SIZE;
 
+ctx.fillStyle="white";
+ctx.fillRect(0, 0, CANAVS_SIZE, CANAVS_SIZE);
 ctx.strokeStyle = INITIAL_COLOR;
 ctx.fillStyle= INITIAL_COLOR;
 ctx.lineWidth = 2.5;
@@ -64,6 +67,17 @@ function handleCanvasClick() {
     }
   }
 
+function handleCM(event){
+    event.preventDefault();
+}
+
+function  handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS[🎨]";
+    link.click();
+}
 
 if(canvas){
     canvas.addEventListener("mousemove", onMouseMove);
@@ -71,8 +85,8 @@ if(canvas){
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
     canvas.addEventListener("click", handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
 }
-
 Array.from(colors).forEach(color => 
     color.addEventListener("click", handleColorClike)
 );
@@ -82,10 +96,12 @@ if (range) {
 }
 
 if(mode){
-    mode.addEventListener("click", handleModeClike)
+    mode.addEventListener("click", handleModeClike);
 }
 
-
+if(saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
+}
 
 
 
